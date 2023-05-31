704dcaf8624dd30bd52d11d9e24d2b249d839207
IlMane <133958218+IlMane06@users.noreply.github.com>
2023-05-31 09:47:35 +0200

    Add files via upload

--- README.md
+++ README.md
@@ -2,3 +2,9 @@
 Benvenuti nel sito ufficiale del superlussuoso hotel appena inaugurato a Dubai
 
 Tutti i riferimenti, immagini e collegamenti del sito sono inseriti solo a scopo didattico e sono completamente irrealistici.
+
+
+-aggiungi tasto home su blackzone
+-pasti
+-zona
+-stanze+costi
--- script.js
+++ script.js
@@ -1,8 +1,21 @@
+window.onscroll = function() {scrollFunction()};
 
-var btn1 = document.getElementById("btn");
+function scrollFunction() {
 
-function btn1() {
+    if (document.body.scrollTop > 300 || document.documentElement.scrollTop > 300) {
+      document.getElementById("blackzone").style.height = "5%";
+      document.getElementById("logo").style.width = "10%";
+      document.getElementById("logo").style.height = "5%";
+    } else if (document.body.scrollTop > 150 || document.documentElement.scrollTop > 150) {
+      document.getElementById("blackzone").style.height = "10%";
+      document.getElementById("logo").style.width = "20%";
+      document.getElementById("logo").style.height = "10%";
+    } else {
+        document.getElementById("blackzone").style.height = "20%";
+        document.getElementById("logo").style.width = "30%";
+        document.getElementById("logo").style.height = "20%";
+    }
 
-    action
+    
 
-}
+  }
